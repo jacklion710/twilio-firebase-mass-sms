7654ac57be7b5cc8252e97062a8aaa6005eca4d9
jacklion710 <jack.lion710@gmail.com>
2024-02-08 18:04:14 -0500

    Delay in between messages to avoid rate limits

--- pages/api/twilio.ts
+++ pages/api/twilio.ts
@@ -1,15 +1,16 @@
-// /pages/api/twilio.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import twilio from 'twilio';
 
-// Assuming you have the Twilio client initialized as before
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_SECRET_KEY);
 
-const prepareMessage = (template: string, user: {firstName: string, lastName: string}): string => {
+const prepareMessage = (template: string, user: { firstName: string, lastName: string }): string => {
   return template
     .replace(/{firstName}/g, user.firstName)
     .replace(/{lastName}/g, user.lastName);
 };
+
+// Function to introduce a delay
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -18,13 +19,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       for (const user of users) {
         const personalizedMessage = prepareMessage(template, user);
-        
-        // Sending the personalized message to each user
+
         await client.messages.create({
           body: personalizedMessage,
-          to: user.phoneNumber, // Assuming each user object has a phoneNumber
+          to: user.phoneNumber,
           from: process.env.TWILIO_PHONE_NUMBER,
         });
+
+        // Wait for a specified delay before sending the next message
+        // Adjust the delay as necessary to comply with Twilio's rate limits and your usage patterns
+        await delay(5000); // Delay of 1 second (1000 milliseconds)
       }
 
       res.status(200).json({ success: true, message: 'Messages sent successfully!' });
