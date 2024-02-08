247b72f9404dd23dbd7669c4692c2439a59e8650
jacklion710 <jack.lion710@gmail.com>
2024-02-08 17:51:22 -0500

    Enhanced stlying

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import {
   Box,
   Container,
   Center,
+  Flex,
   Textarea,
   Button,
+  Heading,
   VStack,
   Text,
   useDisclosure,
@@ -17,7 +19,16 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  theme as baseTheme,
+  extendTheme,
 } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Kdam Thmor Pro', sans-serif",
+    body: "'Kdam Thmor Pro', sans-serif",
+  },
+}, baseTheme);
 
 interface User {
   firstName: string;
@@ -61,49 +72,140 @@ function Home() {
   };
 
   return (
-    <ChakraProvider>
-      <Center minH="100vh" bg="gray.100">
+    <ChakraProvider theme={theme}>
+      <link href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&display=swap" rel="stylesheet"></link>
+
+      <Center minH="100vh" bgColor="#FAF8F5">
         <Container>
-          <Box p={5} bg="white" shadow="md" borderRadius="lg">
+
+          <Heading mb={10} textAlign={'center'}>Mass SMS Builder</Heading>
+
+          <Box 
+            shadow="md" 
+            borderRadius="lg" 
+            minH="600px"
+            rounded={'lg'}
+            boxShadow={'lg'}
+            p={9}
+            bg={'#A8BACB'}
+            w={'32vw'}
+            minW="50vw"
+            alignSelf={'center'}
+            borderColor={'#2E4A6B'}
+            borderWidth="3px"
+          >    
+
+            <Text color="white">List of users who are opted in for texts:</Text>
+
             {/* LIST OF USERS WHO ARE OPTED INTO YOUR SMS LIST */}
-            <Box mt={4} overflowY="scroll" maxH="300px" p={4} border="1px" borderColor="gray.200" borderRadius="md">
+            <Box borderColor="black" bgColor="white" mb={8} mt={4} overflowY="scroll" maxH="400px" p={4} border="1px" borderRadius="md" minH="200px"> 
               <VStack align="stretch">
                 {users.map((user, index) => (
-                  <Text key={index}>{`${user.firstName} ${user.lastName} (${user.phoneNumber})`}</Text>
+                  <Text color='#2E4A6B' key={index}>{`${user.firstName} ${user.lastName} (${user.phoneNumber})`}</Text>
                 ))}
               </VStack>
             </Box>
 
+            <Text color="white">Compose your message:</Text>
+
             {/* MESSAGE TO SEND TO YOUR USERS */}
             <Textarea
-              placeholder="Enter your message here..."
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               mt={4}
+              minH="200px"
+              placeholder="Type your message..."
+              sx={{
+                  '::placeholder': {
+                  color: 'gray.400', 
+                  },
+              }}
+              rows={4} 
+              required 
+              bg="white" 
+              borderColor="black"
+              color='#2E4A6B'
+              _hover={{ borderColor: '#2E4A6B', borderWidth: "2px" }}
+              _focus={{ borderColor: '#FF8C42', borderWidth: '3px' }}
             />
 
             {/* SUBMIT MESSAGE */}
-            <Button mt={4} colorScheme="blue" onClick={handleSendClick}>
-              Send Message
-            </Button>
+            <Flex justifyContent="center" mt={4}> {/* Centered horizontally */}
+              <Button  
+                onClick={handleSendClick}
+                color={'#FFFFFF'}
+                _hover={{
+                  bg: `${'#FF8C42'}80`,
+                  transform: 'scale(1.05)'
+                }}
+                _active={{
+                  bg: `${'#FF8C42'}80`
+                }}
+                boxShadow="0px 4px 10px rgba(0, 0, 0, 0.2)"
+                border="2px solid"
+                borderColor={'#8D8D8D'}
+                backgroundColor={`${'#7C9EB2'}80`}
+                transition="all 0.3s ease-in-out"
+                style={{ borderRadius: '20px' }}
+                mb={4}
+                w="8vw"
+                minW={"130px"}
+              >
+                Send Message
+              </Button>
+            </Flex>
 
             {/* CONFIRMATION MODAL */}
             <Modal isOpen={isOpen} onClose={onClose}>
               <ModalOverlay />
-              <ModalContent>
+              <ModalContent 
+                bgColor={'#A8BACB'} 
+                borderColor={'#2E4A6B'} 
+                borderWidth="3px" 
+                minW="40vw"
+              >
                 <ModalHeader>Confirm Message</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
                   Are you sure you want to send this message?
                 </ModalBody>
                 <ModalFooter>
-                  <Button colorScheme="blue" mr={3} onClick={sendMessage}>
+                  <Button 
+                    mr={3} 
+                    onClick={sendMessage}
+                    bgColor={`${'#6A8D7F'}80`}
+                    color="white" 
+                    _hover={{ bg: `${'#87A696'}80`, transform: 'scale(1.05)' }} 
+                    _active={{ bg: "green.700" }} 
+                    boxShadow="0px 4px 10px rgba(0, 0, 0, 0.2)" 
+                    border="2px solid"
+                    borderColor="green.500"
+                    transition="all 0.3s ease-in-out" 
+                    borderRadius="20px" 
+                    fontFamily="'Kdam Thmor Pro', sans-serif"
+                  >
                     Yes, Send It
                   </Button>
-                  <Button variant="ghost" onClick={onClose}>Cancel</Button>
+                  <Button 
+                    variant="ghost" 
+                    onClick={onClose}
+                    bgColor={`${'#7C9EB2'}80`}
+                    color={'#FFFFFF'}
+                    _hover={{ bg: `${'#FF8C42'}80`, transform: 'scale(1.05)' }}
+                    _active={{ bg: `${'#FF5513'}80` }}
+                    boxShadow="0px 4px 10px rgba(0, 0, 0, 0.2)"
+                    border="2px solid"
+                    borderColor={'#8D8D8D'}
+                    transition="all 0.3s ease-in-out"
+                    borderRadius="20px"
+                    fontFamily="'Kdam Thmor Pro', sans-serif"
+                  >
+                    Cancel
+                  </Button>
                 </ModalFooter>
               </ModalContent>
             </Modal>
+
           </Box>
         </Container>
       </Center>
