5073ec36e5bc6ffab2cd51eeeb240e4e6b092cce
jacklion710 <jack.lion710@gmail.com>
2024-02-08 19:52:31 -0500

    Palette for UI

---
+++ utils/palette.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+    text: "#101C2A",        // Deep blue for text
+    secondaryText: "#FFFFFF", // White for contrastin text
+    background: "#FAF8F5",  // Off-white background for a minimalist touch
+    primary: "#2E4A6B",     // Navy blueish as the primary color
+    secondary: "#A8BACB",   // Muted light-blue as the secondary color
+    accent: "#FF8C42",      // Orange accent color
+    neonAccent: "#FF5513",  // An alternative orange neon for variation
+    accentLight: "#FF9C92",  // A lighter alternative orange neon for variation
+    buttonCol: "#7C9EB2", // Muted blue-grey for less prominent text and buttons
+    grey: "#8D8D8D",        // Neutral grey for secondary elements like borders
+    greenPrimary: "#6A8D7F",  // Muted green color for confirmations and success
+    greenSecondary: "#87A696",  // Subdued green color to accent affirmatives
+    redPrimary: "#BF616A",  // Subdued red color for deleting and errors
+    redSecondary: "#C98C8C",  // Muted red color to accent destructive actions
+};
+  
+// TO USE, CALL: import { COLORS } from '../utils/palette';
