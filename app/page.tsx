f268660c7a21ea062b5b4f6a138773b15a3bc9be
jacklion710 <jack.lion710@gmail.com>
2024-02-08 20:40:24 -0500

    Conditionally render log in or massSMS pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 "use client";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from '@/components/Login';
 import MassSMS from '@/components/MassSMS';
-import {
-  ChakraProvider,
-} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { auth } from '@/utils/firebase';
+import { User } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+// Import User type from Firebase if not already imported
 
 function Home() {
+  // Specify the user state can be User type or null
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser); // currentUser is User | null, which matches the state type
+    });
+
+    return () => unsubscribe(); // Cleanup subscription on unmount
+  }, []);
 
   return (
     <ChakraProvider>
-
-      <MassSMS />
-
+      {user ? <MassSMS /> : <Login />}
     </ChakraProvider>
   );
 }
